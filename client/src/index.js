c43b0348be69ba7818f937976fcdbad2abfd5c33
mehtadeep007 <mehtadeep2013@gmail.com>
2021-04-14 00:21:10 +0530

    heroku deploy

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
-// import { ContextProvider } from './Context';
+import { ContextProvider } from './Context';
 
 import './styles.css';
 
 ReactDOM.render(
-    <App />,
+  <ContextProvider>
+    <App />
+  </ContextProvider>,
   document.getElementById('root'),
 );
